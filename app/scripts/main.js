1071f155587f22c7f906a75d5980b7c19aace96a
shyr <sungho.moon@navercorp.com>
2016-04-05 03:38:36 +0900

    aa

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,11 +5,27 @@ $(function() {
 	var controller = new ScrollMagic.Controller();
 
 // build tween
-	var tween = TweenMax.to("#animation", 0.5, {scale: 1.3, repeat: 5, yoyo: true});
+	/*var tween = TweenMax.to("#animation", 0.5, {
+		opacity: 0/!*,
+		scale: 0,
+		repeat: 0,
+		top: 600*!/
+	});*/
+
+	var tween2 = TweenMax.to("#cover", 1, {
+		opacity: 0.8
+	});
 
 // build scene and set duration to window height
-	var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: "100%"})
-		.setTween(tween)
+	/*var scene = new ScrollMagic.Scene({
+		duration: "100%"
+	}).setTween(tween)
+		.addIndicators() // add indicators (requires plugin)
+		.addTo(controller);*/
+
+	var scene = new ScrollMagic.Scene({
+		duration: "100%"
+	}).setTween(tween2)
 		.addIndicators() // add indicators (requires plugin)
 		.addTo(controller);
 });
