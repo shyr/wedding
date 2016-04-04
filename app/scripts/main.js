378afc2d4b21edf3b42fe0a43cb5d0cf4f8142af
shyr <sungho.moon@navercorp.com>
2016-04-05 00:15:26 +0900

    scrollmagic run well

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,15 @@
-
-
-console.log('\'Allo \'Allo!');
+'use strict';
 
 $(function() {
-	// Init Controller
-	var scrollMagicController = new ScrollMagic();
+// init controller
+	var controller = new ScrollMagic.Controller();
+
+// build tween
+	var tween = TweenMax.to("#animation", 0.5, {scale: 1.3, repeat: 5, yoyo: true});
+
+// build scene and set duration to window height
+	var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: "100%"})
+		.setTween(tween)
+		.addIndicators() // add indicators (requires plugin)
+		.addTo(controller);
 });
