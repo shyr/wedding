8a084b3b819f28325bcd3a39c332c511eddc2f7f
shyr <sungho.moon@navercorp.com>
2016-04-06 04:27:26 +0900

    Update 2016-04-05T19:27:24.202Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,111 @@
-"use strict";$(function(){var e=new ScrollMagic.Controller,a=TweenMax.to("#cover",1,{opacity:.6});new ScrollMagic.Scene({duration:"250px",offset:"170px"}).setTween(a).addTo(e);var n=TweenMax.to("#wrap_confession",1,{color:"rgba(0, 0, 0, 1)"});new ScrollMagic.Scene({triggerElement:"#trigger2",duration:"250px",offset:"-100px"}).setTween(n).addTo(e);var o=TweenMax.to("#wrap_confession",1,{color:"rgba(0, 0, 0, 0.3)"});new ScrollMagic.Scene({triggerElement:"#trigger3",duration:"250px",offset:"-100px"}).setTween(o).addTo(e);var t=TweenMax.to("#location",1,{color:"rgba(0, 0, 0, 1)",opacity:1});new ScrollMagic.Scene({triggerElement:"#trigger4",duration:"250px",offset:"-100px"}).setTween(t).addTo(e);var o=TweenMax.to("#location",1,{color:"rgba(0, 0, 0, 0.3)",opacity:.3});new ScrollMagic.Scene({triggerElement:"#trigger5",duration:"250px",offset:"-100px"}).setTween(o).addTo(e);var i=new nhn.api.map.LatLng(37.3624968,127.1075447),r=new nhn.api.map.LatLng(37.3597611,127.1052704);nhn.api.map.setDefaultPoint("LatLng");var p=new nhn.api.map.Map("div_map_box",{point:i,zoom:10,enableWheelZoom:!0,enableDragPan:!0,enableDblClickZoom:!1,mapMode:0,activateTrafficMap:!1,activateBicycleMap:!1,minMaxLevel:[1,14]}),c=new nhn.api.map.ZoomControl;p.addControl(c),c.setPosition({top:10,left:10});var l=new nhn.api.map.MapTypeBtn;p.addControl(l),l.setPosition({bottom:10,right:80});var s=new nhn.api.map.TrafficMapBtn;s.setPosition({bottom:10,right:150}),p.addControl(s);var g=new nhn.api.map.Size(28,37),d=new nhn.api.map.Size(14,37),w=new nhn.api.map.Icon("http://static.naver.com/maps2/icons/pin_spot2.png",g,d),m=new nhn.api.map.Marker(w,{title:"그린팩토리"});m.setPoint(r),p.addOverlay(m),$(".rslides").responsiveSlides({auto:!0,speed:1500,timeout:1e3})});
+'use strict';
+
+$(function () {
+	// init controller
+	var controller = new ScrollMagic.Controller();
+
+	var dimmer = TweenMax.to("#cover", 1, {
+		opacity: 0.6
+	});
+
+	new ScrollMagic.Scene({
+		duration: '250px',
+		offset: '170px'
+	}).setTween(dimmer).addTo(controller);
+
+	var font_brighter = TweenMax.to("#wrap_confession", 1, {
+		color: 'rgba(0, 0, 0, 1)'
+	});
+
+	new ScrollMagic.Scene({
+		triggerElement: '#trigger2',
+		duration: '250px',
+		offset: '-100px'
+	}).setTween(font_brighter).addTo(controller);
+
+	var font_dimmer = TweenMax.to("#wrap_confession", 1, {
+		color: 'rgba(0, 0, 0, 0.3)'
+	});
+
+	new ScrollMagic.Scene({
+		triggerElement: '#trigger3',
+		duration: '250px',
+		offset: '-100px'
+	}).setTween(font_dimmer).addTo(controller);
+
+	var map_font_brighter = TweenMax.to("#location", 1, {
+		color: 'rgba(0, 0, 0, 1)',
+		opacity: 1
+	});
+
+	new ScrollMagic.Scene({
+		triggerElement: '#trigger4',
+		duration: '250px',
+		offset: '-100px'
+	}).setTween(map_font_brighter).addTo(controller);
+
+	var font_dimmer = TweenMax.to("#location", 1, {
+		color: 'rgba(0, 0, 0, 0.3)',
+		opacity: 0.3
+	});
+
+	new ScrollMagic.Scene({
+		triggerElement: '#trigger5',
+		duration: '250px',
+		offset: '-100px'
+	}).setTween(font_dimmer).addTo(controller);
+
+	/* 지도 */
+	var centerPoint = new nhn.api.map.LatLng(37.3624968, 127.1075447);
+	var oPoint = new nhn.api.map.LatLng(37.3597611, 127.1052704);
+	nhn.api.map.setDefaultPoint('LatLng');
+	var oMap = new nhn.api.map.Map('div_map_box', {
+		point: centerPoint,
+		zoom: 10,
+		enableWheelZoom: true,
+		enableDragPan: true,
+		enableDblClickZoom: false,
+		mapMode: 0,
+		activateTrafficMap: false,
+		activateBicycleMap: false,
+		minMaxLevel: [1, 14]
+	});
+
+	var oSlider = new nhn.api.map.ZoomControl();
+	oMap.addControl(oSlider);
+	oSlider.setPosition({
+		top: 10,
+		left: 10
+	});
+
+	var oMapTypeBtn = new nhn.api.map.MapTypeBtn();
+	oMap.addControl(oMapTypeBtn);
+	oMapTypeBtn.setPosition({
+		bottom: 10,
+		right: 80
+	});
+
+	var trafficButton = new nhn.api.map.TrafficMapBtn(); // - 실시간 교통지도 버튼 선언
+	trafficButton.setPosition({
+		bottom: 10,
+		right: 150
+	}); // - 실시간 교통지도 버튼 위치 지정
+
+	oMap.addControl(trafficButton);
+	// Marker  생성
+	var oSize = new nhn.api.map.Size(28, 37);
+	var oOffset = new nhn.api.map.Size(14, 37);
+	var oIcon = new nhn.api.map.Icon('http://static.naver.com/maps2/icons/pin_spot2.png', oSize, oOffset);
+
+	var oMarker = new nhn.api.map.Marker(oIcon, { title: '그린팩토리' });
+	oMarker.setPoint(oPoint);
+	oMap.addOverlay(oMarker);
+
+	/* 갤러리 */
+	$('.rslides').responsiveSlides({
+		auto: true,
+		speed: 1500,
+		timeout: 1000
+	});
+});
+//# sourceMappingURL=main.js.map
